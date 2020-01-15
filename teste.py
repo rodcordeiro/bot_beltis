106d48ae6ca2a4c9e6475b5ef44209261ab2c736
RODRIGO CORDEIRO <rodrigocordeiro@uni9.edu.br>
2020-01-15 13:05:50 -0300

    Initialize repo

---
+++ teste.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Author: Rodrigo Cordeiro
+
+file = print("c:\\Users\\TI\\Desktop\\Cordeiro\\Rod\\dev\\projeto\\bot_akta\\ping.txt","w")
+file.close()
+file.write("Teste")
+print(open("ping.txt","r").read())
